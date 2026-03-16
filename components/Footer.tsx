ab801def0c5ba27399c78b3712c9ebea21fa93ce
ermktdd8933 <ewingyang520@gmail.com>
2026-03-16 14:23:49 +0800

    二维码

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
 
             {/* Bottom-left: WeChat QR placeholder + small logo */}
             <div className="mt-8 flex items-center gap-4">
-              <img src="https://static.notiontheme.com/qr_code.jpg" alt="微信群二维码" className="w-20 h-20 rounded-xl ring-1 ring-black/5 object-cover" />
+              <img src="https://static.notiontheme.com/qr_code.jpg" alt="微信群二维码" className="w-28 rounded-xl ring-1 ring-black/5" />
             </div>
           </div>
 
